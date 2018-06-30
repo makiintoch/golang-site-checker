ed35f0832b612566b5008b3c2f5934bef8a53307
makiintoch <makiintoch@gmail.com>
2018-06-30 21:06:45 +0300

    add polling function

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func doSomething() {
+	fmt.Println("do something every 2 minutes")
+}
+
+func startPolling() {
+	for {
+		time.Sleep(2 * time.Minute)
+
+		doSomething()
+	}
+}
+
 func main() {
-	fmt.Println("Initial commit")
+	startPolling()
 }
