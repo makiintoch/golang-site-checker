cf8cfeccab689e613aa32ced09cf39ab45de0fae
makiintoch <makiintoch@gmail.com>
2018-07-01 12:42:58 +0300

    get status code

--- main.go
+++ main.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
-func doSomething() {
-	fmt.Println("do something every 2 minutes")
+func getStatusCode() int {
+	resp, err := http.Get("https://google.com/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp.StatusCode
 }
 
 func startPolling() {
 	for {
-		time.Sleep(2 * time.Minute)
+		time.Sleep(1 * time.Minute)
 
-		doSomething()
+		code := getStatusCode()
+
+		if code < 200 || code > 299 {
+			fmt.Println("Argh! Broken: ", code)
+		} else {
+			fmt.Println("All ok: ", code)
+		}
 	}
 }
 
 func main() {
-	startPolling()
+	defer startPolling()
 }
